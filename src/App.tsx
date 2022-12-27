617a361c27b8f2655da087954ac2d09c85f787ed
Emiliano Acerbi <eacerbip@gmail.com>
2022-12-27 18:43:11 -0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,28 @@ import { Notification } from './types'
 function App() {
   return (
     <div className="font-primary">
-      <header>
+      <header className="flex items-center">
         <h1>Notifications</h1>
         <p>Mark all as read</p>
       </header>
 
-      <main>
+      <main className="bg-neutral-50">
         {data.map((notification: Notification) => (
-          <h1 key={notification.author}>{notification.author}</h1>
+          <div key={notification.author} className="flex items-center gap-3">
+            <img src={notification.avatar} alt={notification.author} />
+            <div>
+              <h3 className="inline font-black">{notification.author} </h3>
+              <span className="text-neutral-600">{notification.action}</span>
+              <span className="text-neutral-600 font-extrabold">
+                {' '}
+                {notification.post}
+              </span>
+              <div className="w-3 h-3 inline bg-primary-400 rounded-full" />
+              <span className="">{notification.group}</span>
+              <p className="text-neutral-400">{notification.date}</p>
+              <p>{notification.message}</p>
+            </div>
+          </div>
         ))}
       </main>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        primary: "'Plus Jakarta Sans', sans-serif"
+        primary: "'Plus Jakarta Sans', sans-serif",
+      },
+      colors: {
+        'primary-400': 'hsl(1, 90%, 64%)',
+        'primary-800': 'hsl(219, 85%, 26%)',
+        'neutral-50': 'hsl(210, 60%, 98%)',
+        'neutral-100': 'hsl(211, 68%, 94%)',
+        'neutral-150': 'hsl(205, 33%, 90%)',
+        'neutral-400': 'hsl(219, 14%, 63%)',
+        'neutral-600': 'hsl(219, 12%, 42%)',
+        'neutral-900': 'hsl(224, 21%, 14%)',
       },
     },
   },
