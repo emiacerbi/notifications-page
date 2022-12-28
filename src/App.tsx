0040d1a9926fe36589c48fe705788aed04a91ee7
Emiliano Acerbi <eacerbip@gmail.com>
2022-12-28 11:19:12 -0300

    notifications as state

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,66 @@
+import { useEffect, useState } from 'react'
 import data from './data.json'
 import { Notification } from './types'
 
 function App() {
-  return (
-    <div className="font-primary">
-      <header className="flex items-center">
-        <h1>Notifications</h1>
-        <p>Mark all as read</p>
-      </header>
+  const [notifications, setNotifications] = useState<Notification[]>([])
 
-      <main className="bg-neutral-50">
-        {data.map((notification: Notification) => (
-          <div key={notification.author} className="flex items-center gap-3">
-            <img src={notification.avatar} alt={notification.author} />
-            <div>
-              <h3 className="inline font-black">{notification.author} </h3>
-              <span className="text-neutral-600">{notification.action}</span>
-              <span className="text-neutral-600 font-extrabold">
-                {' '}
-                {notification.post}
-              </span>
-              <div className="w-3 h-3 inline bg-primary-400 rounded-full" />
-              <span className="">{notification.group}</span>
-              <p className="text-neutral-400">{notification.date}</p>
-              <p>{notification.message}</p>
+  useEffect(() => {
+    setNotifications(data)
+  }, [])
+
+  return (
+    <div className="font-primary min-h-screen">
+      <div className="mx-auto max-w-2xl p-5">
+        <header className="flex items-center justify-between">
+          <h1>Notifications</h1>
+          {/* {notifications} */}
+          <p>Mark all as read</p>
+        </header>
+
+        <main className="bg-neutral-50 flex flex-col gap-3">
+          {notifications.map((notification) => (
+            <div key={notification.author} className="flex items-start gap-3">
+              <img
+                src={notification.avatar}
+                className="w-10 h-10 rounded-full"
+                alt={notification.author}
+              />
+              <div>
+                <h3 className="inline font-black">{notification.author} </h3>
+                <span className="text-neutral-600">{notification.action} </span>
+
+                <span className="text-neutral-600 font-extrabold">
+                  {' '}
+                  {notification.post}
+                </span>
+                <span className="text-primary-800 font-bold">
+                  {notification.group}{' '}
+                </span>
+
+                {notification.isActive && (
+                  <div className="w-2 h-2 ml-1 mb-[1px] bg-primary-400 rounded-full inline-block" />
+                )}
+
+                <p className="text-neutral-400">{notification.date}</p>
+
+                {notification.message && (
+                  <p className="border p-3 rounded-md leading-tight text-neutral-600">
+                    {notification.message}
+                  </p>
+                )}
+              </div>
+              {notification.picture && (
+                <img
+                  src={notification.picture}
+                  className="w-10 h-10 rounded-md"
+                  alt={notification.author}
+                />
+              )}
             </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </div>
     </div>
   )
 }
